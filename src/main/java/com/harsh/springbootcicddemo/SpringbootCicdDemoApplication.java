2f4e398885dfb54d08f3bc4f3caeccc58407d184
tripathiharsh95 <tripathiharsh088@gmail.com>
2026-07-15 08:32:06 +0530

    First Commit

---
+++ src/main/java/com/harsh/springbootcicddemo/SpringbootCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.springbootcicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCicdDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harsh/springbootcicddemo/SpringbootCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.springbootcicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
